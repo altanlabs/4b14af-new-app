7e8c11f31322fa5f92cf4d4cd2585d54b59ccee5
altanlabs <interfaces@altan.ai>
2025-02-05 15:16:38 +0000

    New commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '../components/ui/toaster';
 
 export default function HomePage() {
   return (
-    <main className="bg-black text-white pt-16">
+    <main className="bg-black text-white">
       <Hero />
       <About />
       <Portfolio />
